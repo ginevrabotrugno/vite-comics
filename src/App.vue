200275d06bb704d0b067f09f4d0a3b2776ee5570
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-07-05 17:14:19 +0200

    feat: add assets and components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+  }
+
+}
 </script>
 
 <template>
-
+  <AppHeader />
 </template>
 
 <style lang="scss">
