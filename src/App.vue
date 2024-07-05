c13f6f3be59088392a57f2fb589b2ca802364e1b
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-07-05 16:44:45 +0200

    build: create sass files

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script>
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
@@ -14,17 +14,6 @@ import HelloWorld from './components/HelloWorld.vue'
   <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+@use "./styles/general.scss" as *;
 </style>
