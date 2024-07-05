6ab758a43bcfea5b5a23c4c2203c96c637a2001e
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-07-05 17:51:57 +0200

    feat: add header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,70 +1,103 @@
 <script>
 export default {
     name: "AppHeader",
+
+    data() {
+        return {
+            links: [
+                {
+                    text: "CHARACTERS",
+                    url: "#",
+                },
+                {
+                    text: "COMICS",
+                    url: "#",
+                },
+                {
+                    text: "MOVIES",
+                    url: "#",
+                },
+                {
+                    text: "TV",
+                    url: "#",
+                },
+                {
+                    text: "GAMES",
+                    url: "#",
+                },
+                {
+                    text: "COLLECTIBLES",
+                    url: "#",
+                },
+                {
+                    text: "VIDEOS",
+                    url: "#",
+                },
+                {
+                    text: "FANS",
+                    url: "#",
+                },
+                {
+                    text: "NEWS",
+                    url: "#",
+                },
+                {
+                    text: "SHOP",
+                    url: "#",
+                },
+
+            ]
+        }
+    }
 }
 </script>
 
 <template>
     <header>
         <div class="container">
-            <a href="#">
+            <a href="#" class="logo">
                 <img src="../assets/dc-logo.png" alt="Logo">
             </a>
 
-        <nav>
-            <ul>
-                <li>
-                    <a href="#">Voce Menu</a>
-                </li>
-                <li>
-                    <a href="#">Voce Menu</a>
-                </li>
-                <li>
-                    <a href="#">Voce Menu</a>
-                </li>
-                <li>
-                    <a href="#">Voce Menu</a>
-                </li>
-                <li>
-                    <a href="#">Voce Menu</a>
-                </li>
-                <li>
-                    <a href="#">Voce Menu</a>
-                </li>
-                <li>
-                    <a href="#">Voce Menu</a>
-                </li>
-                <li>
-                    <a href="#">Voce Menu</a>
-                </li>
-                <li>
-                    <a href="#">Voce Menu</a>
-                </li>
-                <li>
-                    <a href="#">Voce Menu</a>
-                </li>
-
-            </ul>
-        </nav>
+            <nav>
+                <ul>
+                    <li v-for="(link, index) in links" :key="index">
+                        <a :href="link.url">
+                            {{ link.text }}
+                        </a>
+                    </li>
+                </ul>
+            </nav>
 
         </div>
     </header>
 </template>
 
-<style lang="scss">
-    header {
-        padding: 20px 0;
+<style lang="scss" scoped>
+
+    header{
+        height: 100px;
     }
 
     .container {
         display: flex;
         justify-content: space-between;
         align-items: center;
+        height: 100%;
+
+        .logo {
+        width: 50px;
+        aspect-ratio: 1/1;
+        }
+
+        ul {
+            display: flex;
+            gap: 20px;
+            align-items: center;
+
+        }
+
     }
 
-    ul {
-        display: flex;
-        gap: 20px;
-        align-items: center;
-    }
+    
 </style>
