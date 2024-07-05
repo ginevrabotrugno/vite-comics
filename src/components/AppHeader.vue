51c9bf5357510856ef5ea0d296a82e77c7251bb1
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-07-05 17:27:03 +0200

    feat: add  base app header structure and style

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,8 +5,66 @@ export default {
 </script>
 
 <template>
+    <header>
+        <div class="container">
+            <a href="#">
+                <img src="../assets/dc-logo.png" alt="Logo">
+            </a>
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Voce Menu</a>
+                </li>
+                <li>
+                    <a href="#">Voce Menu</a>
+                </li>
+                <li>
+                    <a href="#">Voce Menu</a>
+                </li>
+                <li>
+                    <a href="#">Voce Menu</a>
+                </li>
+                <li>
+                    <a href="#">Voce Menu</a>
+                </li>
+                <li>
+                    <a href="#">Voce Menu</a>
+                </li>
+                <li>
+                    <a href="#">Voce Menu</a>
+                </li>
+                <li>
+                    <a href="#">Voce Menu</a>
+                </li>
+                <li>
+                    <a href="#">Voce Menu</a>
+                </li>
+                <li>
+                    <a href="#">Voce Menu</a>
+                </li>
+
+            </ul>
+        </nav>
+
+        </div>
+    </header>
 </template>
 
 <style lang="scss">
+    header {
+        padding: 20px 0;
+    }
 
+    .container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    ul {
+        display: flex;
+        gap: 20px;
+        align-items: center;
+    }
 </style>
