034751707ae560193582287618c2d5c9777aa0fb
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-07-06 20:40:15 +0200

    feat: add app content component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script>
 import AppHeader from './components/AppHeader.vue'
+import AppContent from './components/AppContent.vue'
 
 export default {
   name: "App",
   components: {
     AppHeader,
+    AppContent,
   }
 
 }
@@ -12,6 +14,7 @@ export default {
 
 <template>
   <AppHeader />
+  <AppContent/>
 </template>
 
 <style lang="scss">
---
+++ src/components/AppContent.vue
@@ -0,0 +1,24 @@
+<script>
+
+export default {
+    name: "AppContent",
+}
+
+</script>
+
+<template>
+    <section>
+        <div class="container">
+            <h1> CONTENT GOES HERE </h1>
+        </div>
+    </section>
+
+</template>
+
+<style lang="scss" scoped>
+    section {
+        background-color: black;
+        color: white;
+        padding: 50px 0;
+    }
+</style>
