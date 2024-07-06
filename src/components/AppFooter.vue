34aba0abd9c35cc77d337eef59e771d091117515
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-07-06 21:13:25 +0200

    feat: add data to AppFoter

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,134 @@
 <script>
 export default {
     name: "AppFooter",
+
+    data () {
+        return {
+            lists: [
+                {
+                    title: "DC COMICS",
+                    links: [
+                        {
+                            name: "Characters",
+                            url: "#"
+                        },
+                        {
+                            name: "Comics",
+                            url: "#"
+                        },
+                        {
+                            name: "Movies",
+                            url: "#"
+                        },
+                        {
+                            name: "TV",
+                            url: "#"
+                        },
+                        {
+                            name: "Games",
+                            url: "#"
+                        },
+                        {
+                            name: "Videos",
+                            url: "#"
+                        },
+                        {
+                            name: "News",
+                            url: "#"
+                        },
+                    ]
+                },
+                {
+                    title: "DC",
+                    links: [
+                        {
+                            name: "Terms of Use",
+                            url: "#"
+                        },
+                        {
+                            name: "Privacy policy(New)",
+                            url: "#"
+                        },
+                        {
+                            name: "Ad Choices",
+                            url: "#"
+                        },
+                        {
+                            name: "Advertizing",
+                            url: "#"
+                        },
+                        {
+                            name: "Jobs",
+                            url: "#"
+                        },
+                        {
+                            name: "Subscriptions",
+                            url: "#"
+                        },
+                        {
+                            name: "Talent Workshops",
+                            url: "#"
+                        },
+                        {
+                            name: "CPSC Certificates",
+                            url: "#"
+                        },
+                        {
+                            name: "Ratings",
+                            url: "#"
+                        },
+                        {
+                            name: "Shop Help",
+                            url: "#"
+                        },
+                        {
+                            name: "Contact Us",
+                            url: "#"
+                        },
+                    ]
+                },
+                {
+                    title: "SITES",
+                    links: [
+                        {
+                            name: "DC",
+                            url: "#"
+                        },
+                        {
+                            name: "MAD Magazine",
+                            url: "#"
+                        },
+                        {
+                            name: "DC Kids",
+                            url: "#"
+                        },
+                        {
+                            name: "DC Universe",
+                            url: "#"
+                        },
+                        {
+                            name: "DC Power Visa",
+                            url: "#"
+                        },
+                    ]
+                },
+                {
+                    title: "SHOP",
+                    links: [
+                        {
+                            name: "Shop DC",
+                            url: "#"
+                        },
+                        {
+                            name: "Shop DC Collectibles",
+                            url: "#"
+                        },
+                        
+                    ]
+                },
+            ]
+        }
+    }
 }
 </script>
 
@@ -9,13 +137,21 @@ export default {
            <section class="links">
                 <div class="container">
 
+                    <ul v-for="(list, index) in lists" :key="index">
+                        <li>{{ list.title }}
+                            <ul>
+                                <li v-for="(link, index) in list.links" :key="index">
+                                    <a :href="link.url">{{ link.name }}</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+
                 </div>
            </section>
            <section class="sign-up">
                 <div class="container">
-                    <div class="col_50">
 
-                    </div>
                 </div>
            </section>
  
