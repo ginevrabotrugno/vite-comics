fc3acd8bc43d23f908316fa389de33b695743fe2
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-07-08 16:58:12 +0200

    feat: create card component and data

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,18 @@
+<script>
+
+export default {
+    name: "AppCard",
+}
+
+</script>
+
+<template>
+
+    <div class="card">
+        
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+</style>
